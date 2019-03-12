ec5836c71f086abd1f535a9d0b1bc7328fd4727b
atjhoendz <achunarmando@gmail.com>
2019-03-12 21:23:12 +0700

    tugas Analgo 2

---
+++ 3.cpp
@@ -0,0 +1,52 @@
+/*
+Nama	: Mohamad Achun Armando
+Kelas	: B
+NPM 	: 140810170020
+Nama Program : Binary Search
+*/
+	
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	
+	int n, i, arr[100], cari, awal, akhir, tengah;
+	cout << "\n=================================="<<endl;
+	cout<<"Masukkan banyak data : ";cin>>n;
+	cout << "\n----------------------------------" << endl;
+	for (i=0; i<n; i++)
+	{
+		cout<<"Data ke-"<<i+1<<" :";
+		cin>>arr[i];
+	}
+	cout<<"\nMasukkan data yang akan di cari :"; cin>>cari;
+	awal = 0;
+	akhir = n-1;
+	cout << "\n----------------------------------" << endl;
+	while (awal <= akhir)
+	{
+		tengah = (awal+akhir)/2;
+		if(arr[tengah] < cari)
+		{
+			awal = tengah + 1;
+
+		}
+		else if(arr[tengah] == cari)
+		{
+			cout<<cari<<" ditemukan pada data ke-"<<tengah+1<<"\n";
+			break;
+		}
+		else
+		{
+			 akhir = tengah - 1;
+		}
+		tengah = (awal + akhir)/2;
+	}
+	if(awal > akhir)
+	{
+		cout<<cari<<"Tidak Ditemukan! Karena ";
+	}
+	cout << "\n=================================="<<endl;
+	system("read a");
+}
